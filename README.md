a0a87edb37f4c4ac325ef01fd8c827afb42b72e5
Bad Wolf <noel.garcia@openmailbox.org>
2017-10-19 20:31:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Personal Background
+
+In this repo I keep a svg template that gets modified periodically providing a dynamical desktop background.
---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import os
+here = os.path.dirname(os.path.realpath(__file__))
+HOME = os.getenv('HOME')
+
+
+model_svg = here + 'background.svg'
+tmp_png = '/tmp/test.png'
+desk_back = HOME + 'Pictures/background.png'
+
